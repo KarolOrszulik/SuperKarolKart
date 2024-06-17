022a98b2fc7fca5a090199d596e0a48cf243107a
Karol Orszulik <orkarox@gmail.com>
2024-06-18 00:24:28 +0200

    Lekki refactor

--- Kart.cpp
+++ Kart.cpp
@@ -4,18 +4,20 @@
 
 void Kart::handleMovement(float dt)
 {
-	Track::Tile tile = m_track->atPos(m_position);
-
-	float maxSpeed = tile == Track::Tile::GRASS ? 60.f : 120.f;
-	const float deceleration = tile == Track::Tile::GRASS ? 120.f : 90.f;
-	maxSpeed *= m_speedMultiplier;
-
 	constexpr float WHEELBASE = 12.f;
 	constexpr float MAX_FRICTION = 350.f;
+	constexpr float LOCK_ANGLE = 30.f * 3.14f / 180.f; // max steering angle in radians
+
+	Track::Tile tile = m_track->atPos(m_position);
+
+	const float deceleration = tile == Track::Tile::GRASS ? 120.f : 90.f;
+	float maxSpeed = tile == Track::Tile::GRASS ? 60.f : 120.f;
+	maxSpeed *= m_speedMultiplier;
+
 
 	if (m_speed >= maxSpeed || m_speed <= -maxSpeed * 0.4f || std::abs(m_acceleratorInput) < 0.01f)
 	{
-		if (std::abs(m_speed) > 1.f) // only decelerate if speed is not negligible
+		if (std::abs(m_speed) > 1.f)
 		{
 			float dir = m_speed > 0.f ? 1.f : -1.f;
 			m_acceleration = -dir * deceleration;
@@ -38,34 +40,22 @@ void Kart::handleMovement(float dt)
 	}
 
 	m_speed += m_acceleration * dt;
-	//if (tile == Track::Tile::GRASS)
-		// m_speed = std::clamp(m_speed, -m_maxSpeed / m_speedMultiplier, m_maxSpeed / m_speedMultiplier);
-
-	constexpr float LOCK_ANGLE = 30.f * 3.14f / 180.f;
-	//if (std::abs(m_steeringInput) < 0.01f || m_wheelsAngle >= LOCK_ANGLE || m_wheelsAngle <= -LOCK_ANGLE) // no steering input, return to center gradually
-	//{
-	//	m_wheelsAngle = std::lerp(m_wheelsAngle, 0.f, 1.f - std::exp(-10.f * dt));
-	//}
-	//else // steer slowly, much like with throttle
-	//{
-	//	m_wheelsAngle = std::lerp(m_wheelsAngle, m_steeringInput * LOCK_ANGLE, 1.f - std::exp(-5.f * dt));
-	//} // olaæ to, proœciej jest mo¿e mniej realistycznie, ale lepiej siê jeŸdzi
-	float wheelsAngle = m_steeringInput * LOCK_ANGLE;
+	
 
 	const float max_angle = std::atan(WHEELBASE * MAX_FRICTION / (m_speed * m_speed));
-	wheelsAngle = std::clamp(wheelsAngle, -max_angle, max_angle);
 
-	sf::Vector2f rWheel = m_position - getUnitVector() * (WHEELBASE / 2.f);
-	sf::Vector2f fWheel = m_position + getUnitVector() * (WHEELBASE / 2.f);
+	float wheelsAngle = std::clamp(m_steeringInput * LOCK_ANGLE, -max_angle, max_angle);
 
-	rWheel += getUnitVector() * m_speed * dt;
-	float fWheelRotation = m_angle + wheelsAngle;
-	fWheel += sf::Vector2f(std::cos(fWheelRotation), std::sin(fWheelRotation)) * m_speed * dt;
+	sf::Vector2f frontWheelPos = m_position + getUnitVector() * (WHEELBASE / 2.f);
+	sf::Vector2f rearWheelPos  = m_position - getUnitVector() * (WHEELBASE / 2.f);
 
-	sf::Vector2f newDirection = fWheel - rWheel;
+	rearWheelPos += getUnitVector() * m_speed * dt;
+	const float fWheelRotation = m_angle + wheelsAngle;
+	frontWheelPos += sf::Vector2f(std::cos(fWheelRotation), std::sin(fWheelRotation)) * m_speed * dt;
+
+	const sf::Vector2f newDirection = frontWheelPos - rearWheelPos;
 	m_angle = std::atan2(newDirection.y, newDirection.x);
 
-	m_position = rWheel + newDirection / 2.f;
-
+	m_position = rearWheelPos + newDirection / 2.f;
 	m_angle = std::fmod(m_angle, 2 * 3.14f);
 }
