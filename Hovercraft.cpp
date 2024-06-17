db33a5efcb7acaadf6cc25bdd7a4c3b33cba19f9
Karol Orszulik <orkarox@gmail.com>
2024-06-18 00:42:07 +0200

    Ograniczenie prędkości hovercrafta

--- Hovercraft.cpp
+++ Hovercraft.cpp
@@ -2,7 +2,7 @@
 
 void Hovercraft::handleMovement(float dt)
 {
-	constexpr float MAX_SPEED = 100.f;
+	constexpr float MAX_SPEED = 500.f; // speed at which the hovercraft stops accelerating
 	constexpr float MAX_ANGULAR_VELOCITY = 150.f / 180.f * 3.1415f;
 
 	sf::Vector2f force;
@@ -12,9 +12,10 @@ void Hovercraft::handleMovement(float dt)
 		m_velocity.x = std::lerp(m_velocity.x, 0.f, 1 - std::exp(-3.f * dt));
 		m_velocity.y = std::lerp(m_velocity.y, 0.f, 1 - std::exp(-3.f * dt));
 	}
-	else // POWERRRR
+	else
 	{
-		float acceleration = m_acceleratorInput * 80.f;
+		float speed = std::sqrt(m_velocity.x * m_velocity.x + m_velocity.y * m_velocity.y);
+		float acceleration = 180.f * m_acceleratorInput * (1.f - speed / MAX_SPEED);
 		m_velocity += getUnitVector() * acceleration * dt;
 	}
 
