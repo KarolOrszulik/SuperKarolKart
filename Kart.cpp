7fc8d3b8cb5f9d72aacf5dfa06be47ae57f72f8f
Karol Orszulik <orkarox@gmail.com>
2024-06-17 17:18:44 +0200

    Kart jakaś implementacja

--- Kart.cpp
+++ Kart.cpp
@@ -1,16 +1,48 @@
 #include "Kart.h"
 
+#include <iostream>
+
 void Kart::handleMovement(float dt)
 {
 	Track::Tile tile = m_track->atPos(m_position);
 
 	m_maxSpeed = tile == Track::Tile::GRASS ? 60.f : 120.f;
+	m_maxSpeed *= m_speedMultiplier;
 
-	m_angle += m_steeringInput * dt * 3.14f;
+	// calculate speed
+	if (m_acceleratorInput > 0.1f && m_speed < m_maxSpeed) // forward
+	{
+		m_acceleration = 60.f;
+
+		m_speed += m_acceleration * dt;
+	}
+	else if (m_acceleratorInput < -0.1f && m_speed > 0.5f * m_maxSpeed) // reverse
+	{
+		m_acceleration = -120.f;
+	} 
+	else // coast
+	{
+		m_acceleration = 30.f;
+		m_acceleration *= m_speed > 0 ? -1.f : 1.f;
+	}
+	m_acceleration *= m_speedMultiplier;
+	m_speed += m_acceleration * dt;
+
+	// calculate steering
+	const float maxSteeringAngle = std::exp(-m_speed / 100.f);
+	m_wheelsAngle += m_steeringInput * dt * 2.f;
+	m_wheelsAngle = std::clamp(m_wheelsAngle, -maxSteeringAngle, maxSteeringAngle);
+
+	if (std::abs(m_steeringInput) < 0.1f)
+	{
+		m_wheelsAngle -= 1.5f * dt * (m_wheelsAngle > 0 ? 1.f : -1.f);
+	}
+
+	m_angle += m_wheelsAngle * m_speed * dt * 0.06f;
+
 	m_angle = std::fmod(m_angle, 2 * 3.14f);
 
-	if (m_acceleratorInput > 0.0f)
-		m_position += sf::Vector2f(std::cos(m_angle), std::sin(m_angle)) * dt; // to nie dzia³a na razie, naprawie po mergu
+	m_position += getUnitVector() * m_speed * dt;
 
 	m_acceleratorInput = 0.0f;
 	m_steeringInput = 0.0f;
--- Kart.h
+++ Kart.h
@@ -8,12 +8,10 @@ public:
 	using Vehicle::Vehicle;
 
 private:
-	static constexpr float MAX_WHEELS_ANGLE = 30.f;
-
 	void handleMovement(float dt) override;
 	int getTextureOffset() override { return 0; }
 
-	float m_wheelsAngle = 0.f;
+	float m_wheelsAngle = 0.f; // not actual angle, measure from -1 to 1
 	float m_acceleration = 0.f;
 	float m_speed = 0.f;
 	float m_maxSpeed = 0.f;
